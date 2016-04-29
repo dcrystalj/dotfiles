0d94d4312c744d0d6f4147e21cb2f1f1fc8d1533
dcrystalj <dcrystalj@gmail.com>
2016-04-29 23:18:01 +0200

    install file

---
+++ install.sh
@@ -0,0 +1,16 @@
+yes | sudo apt-get install diodon   # clipboard
+yes | sudo apt-get install redshift # flux for linux
+yes | sudo apt-get install xclip    # allows pipe to xclip 
+yes | sudo apt-get install gcp      # copy with progress
+yes | sudo apt-get install zsh      # copy with progress
+yes | sudo apt-get install vim
+
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+
+
+
+
+
+
+
+
